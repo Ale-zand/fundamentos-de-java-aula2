34c07d1311b40e03aaf56632edeb462e23568320
Ale-zand <ale_zand@hotmail.com>
2023-04-04 11:33:44 -0300

    lista duas dimensoes + atualizando, dividindo e juntando listas

---
+++ atualizando-lista.js
@@ -0,0 +1,7 @@
+const nomes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
+
+// nomes.splice(1, 2, 'Rodrigo');
+nomes.splice(1, 2, 'Rodrigo', 'Alexandre', 'Pedro');
+// nomes.push('Rodrigo');  dá pra fazer direto no splice (assim fica no final e com splice no meio)
+
+console.log(nomes);
---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,6 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio'];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+console.log(`A ${listaDeAlunosEMedias[0][1]} tirou nota ${listaDeAlunosEMedias[1][1]}`);
